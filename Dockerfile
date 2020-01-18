6ff5c2ea02c4610410dd40ec89f9713a9dc68c09
Vladimir Zaychikov <zaychikov.vl@gmail.com>
2020-01-18 19:16:40 +0300

    docker works

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,14 @@
 FROM node:13.6.0
 
-RUN npm install -g nodemon
-
 WORKDIR /var/www/interaktiv
+
+COPY ./backend/package*.json ./
+
+RUN npm install -g nodemon
 
 CMD npm install && nodemon index.js
 
-EXPOSE 80
+EXPOSE 3000
 
 # Create Node container
 # docker run -d -v $(pwd)/backend:/var/www/interaktiv -p 3000:3000 --network=interaktiv-network --name=interaktiv node-interaktiv
--- backend/index.js
+++ backend/index.js
@@ -2,20 +2,23 @@ const http = require('http');
 const { Client } = require('pg');
 const client = new Client({
     user: 'postgres',
-    host: '0.0.0.0',
+    host: 'db',
     database: 'postgres',
     password: 'secret',
     port: 5432
 });
 
-
 client.connect();
-
 client
     .query('SELECT * FROM users')
-    .then(res => {
-        console.log(res.rows);
+    .then(rows => {
+        http.createServer((req, res) => {
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.write(JSON.stringify(rows.rows));
+            res.end();
+            
+        }).listen(3000);
+
         client.end();
     })
     .catch(err => console.log(err));
-
