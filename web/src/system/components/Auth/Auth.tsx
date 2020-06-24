a6330fc90d926f1bddeb1a66a70384827e43b260
Vladimir Zaychikov <zaychikov.vl@gmail.com>
2020-06-24 17:56:10 +0300

    доработка стилей

--- web/src/system/components/Auth/Auth.tsx
+++ web/src/system/components/Auth/Auth.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { TextField, Button } from '@material-ui/core';
+import { bem } from '@utils/formatters';
+import './Auth.scss';
+
+const cn = bem('Auth');
 
 const Auth: React.FC = () => (
-    <div className="auth">
-        <form className="auth__form">
+    <div className={cn()}>
+        <form className={cn('form')}>
             <h3>ISKOR Interaktiv</h3>
-            <TextField label="Логин" size="small" variant="outlined"/>
-            <TextField label="Пароль" size="small" variant="outlined"/>
+            <TextField label="Логин" size="small" variant="outlined" />
+            <TextField label="Пароль" size="small" variant="outlined" />
             <Button variant="outlined" color="primary">Войти</Button>
         </form>
     </div>
