7b0d5b80805ce01a64cacbfb3697c29ee54aedf9
Vladimir Zaychikov <zaychikov.vl@gmail.com>
2020-10-27 20:56:18 +0300

    updates

--- jenkins/scripts/docker-stop.sh
+++ jenkins/scripts/docker-stop.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # остановить контейнер бэка
-if [[ -n docker ps --filter "name=backend" -qa ]]; then
+if [[ -n $(docker ps --filter "name=backend" -qa) ]]; then
     docker stop $(docker ps --filter "name=backend" -qa)
 fi
 
 
 # удалить контейнер бэка
-if [[ -n docker ps --filter "name=backend" -qa ]]; then
+if [[ -n $(docker ps --filter "name=backend" -qa) ]]; then
     docker rm $(docker ps --filter "name=backend" -qa)
 fi
