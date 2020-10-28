4e799c7bc7aa2344d04e6ba4ff43b8c3837003ac
Vladimir Zaychikov <zaychikov.vl@gmail.com>
2020-10-28 10:33:09 +0300

    updates

--- jenkins/scripts/docker-stop.sh
+++ jenkins/scripts/docker-stop.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# остановить контейнер бэка
+# остановить и удалить контейнер бэка
 if [[ -n $(docker ps --filter "name=node-backend" -qa) ]]; then
     docker stop $(docker ps --filter "name=node-backend" -qa)
     docker rm $(docker ps --filter "name=node-backend" -qa)
-else
-    docker run -d -p 8000:3000 --name=node-backend zayanimal/node-backend:v1
 fi
