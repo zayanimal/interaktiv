29bfedb458a504c438d9747b142af9ba81a945b4
Vladimir Zaychikov <zaychikov.vl@gmail.com>
2020-10-28 20:56:19 +0300

    updates

--- backend/src/api/routes.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import * as db from './queries';
-
-const router: express.IRouter = express.Router();
-
-router.get('/orders', db.getOrders);
-router.get('/customers', db.getCustomers);
-
-export default router;
