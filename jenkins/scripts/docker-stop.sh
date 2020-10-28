4c9ea4859d01a006b49e1f964da38e6dfd1b738b
Vladimir Zaychikov <zaychikov.vl@gmail.com>
2020-10-28 10:25:48 +0300

    updates

--- jenkins/scripts/docker-stop.sh
+++ jenkins/scripts/docker-stop.sh
@@ -5,5 +5,5 @@ if [[ -n $(docker ps --filter "name=node-backend" -qa) ]]; then
     docker stop $(docker ps --filter "name=node-backend" -qa)
     docker rm $(docker ps --filter "name=node-backend" -qa)
 else
-    docker run -d -p 8000:3000 --name=node-backend zayanimal/node-backend
+    docker run -d -p 8000:3000 --name=node-backend zayanimal/node-backend:v1
 fi
