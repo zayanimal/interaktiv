dfe83e9126d9c330a4091024ccbd45290a7670e0
Vladimir Zaychikov <zaychikov.vl@gmail.com>
2020-10-29 19:42:03 +0300

    updates

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
+// import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { configService } from './config/config.service';
+// import { configService } from './config/config.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
+    // TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
     AuthModule,
     UsersModule
   ],
