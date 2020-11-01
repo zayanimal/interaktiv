b74f17cb281bccb6bf097bc27f84dd586bc2bad7
Vladimir Zaychikov <zaychikov.vl@gmail.com>
2020-11-01 23:03:15 +0300

    docker updates

--- jenkins/scripts/docker-stop.sh
+++ jenkins/scripts/docker-stop.sh
@@ -2,5 +2,5 @@
 
 # остановить контейнер бэка
 if [[ -n $(docker ps --filter "name=nest-backend" -qa) ]]; then
-    docker stop $(docker ps --filter "name=nest-backend" -qa)
+    docker stop nest-backend
 fi
