e7d0ce30559185e9faa020006dd1f352396a9706
Vladimir Zaychikov <zaychikov.vl@gmail.com>
2020-11-21 23:22:06 +0300

    back/front/docker/jenkins

--- jenkins/scripts/backend_build.sh
+++ jenkins/scripts/backend_build.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
-dockerDeploy() {
-    echo "$(docker rmi zayanimal/nest-backend:v1)"
-    echo "$(docker build -t zayanimal/nest-backend:v1 ./backend/.)"
-    echo "$(docker run --name nest-backend --rm --network=interaktiv -d -p 8000:8000 zayanimal/nest-backend:v1)"
-    echo "$(docker images -q -f dangling=true | xargs --no-run-if-empty docker rmi)"
-}
-
 if [[ -n $(docker ps --filter "name=nest-backend" -qa) ]]
 then
     docker stop nest-backend
-    dockerDeploy()
-else
-    dockerDeploy()
 fi
+
+docker rmi zayanimal/nest-backend:v1
+docker build -t zayanimal/nest-backend:v1 ./backend/.
+docker run --name nest-backend --rm --network=interaktiv -d -p 8000:8000 zayanimal/nest-backend:v1
+docker images -q -f dangling=true | xargs --no-run-if-empty docker rmi
--- jenkins/scripts/frontend_build.sh
+++ jenkins/scripts/frontend_build.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
-dockerDeploy() {
-    echo "$(docker rmi zayanimal/react-frontend:v1)"
-    echo "$(docker build -t zayanimal/react-frontend:v1 ./frontend/.)"
-    echo "$(docker run --name react-frontend --rm --network=interaktiv -d -p 8000:8000 zayanimal/react-frontend:v1)"
-    echo "$(docker images -q -f dangling=true | xargs --no-run-if-empty docker rmi)"
-}
-
 if [[ -n $(docker ps --filter "name=react-frontend" -qa) ]]
 then
     docker stop react-frontend
-    dockerDeploy()
-else
-    dockerDeploy()
 fi
+
+docker rmi zayanimal/react-frontend:v1
+docker build -t zayanimal/react-frontend:v1 ./frontend/.
+docker run --name react-frontend --rm --network=interaktiv -d -p 8000:8000 zayanimal/react-frontend:v1
+docker images -q -f dangling=true | xargs --no-run-if-empty docker rmi
