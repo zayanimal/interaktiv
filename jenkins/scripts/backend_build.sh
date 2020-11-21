4c1ec37e9e9b41b9e9e77097f9f89c29ce21ec4e
Vladimir Zaychikov <zaychikov.vl@gmail.com>
2020-11-21 22:59:05 +0300

    back/front/docker/jenkins

--- jenkins/scripts/backend_build.sh
+++ jenkins/scripts/backend_build.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+dockerDeploy() {
+    docker rmi zayanimal/nest-backend:v1
+    docker build -t zayanimal/nest-backend:v1 ./backend/.
+    docker run --name nest-backend --rm --network=interaktiv -d -p 8000:8000 zayanimal/nest-backend:v1
+    docker images -q -f dangling=true | xargs --no-run-if-empty docker rmi
+}
+
+if [[ -n $(docker ps --filter "name=nest-backend" -qa) ]]; then
+    docker stop nest-backend
+    dockerDeploy()
+else
+    dockerDeploy()
+fi
