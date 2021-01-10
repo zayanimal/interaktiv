52514309b57dfc17aceefecedbc590ac734a67fb
Vladimir Zaychikov <zaychikov.vl@gmail.com>
2021-01-10 17:45:08 +0300

    front deploy

--- frontend/src/system/services/rest.service.ts
+++ frontend/src/system/services/rest.service.ts
@@ -3,7 +3,7 @@ import { TokenService } from '@system/services/token.service';
 import { IRestService, IHeader } from '@system/interfaces';
 import { ApiUrl } from '@system/decorators';
 
-@ApiUrl('api')
+@ApiUrl('api/')
 export class RestService implements IRestService {
     constructor(private tokenService: TokenService) {}
 
