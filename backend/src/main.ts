22f97dcb2284a3096b36a2c89539316910daf0e2
Vladimir Zaychikov <zaychikov.vl@gmail.com>
2021-01-10 18:03:56 +0300

    front deploy

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,6 +7,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
           app.enableCors();
+          app.setGlobalPrefix('api');
 
     const options = new DocumentBuilder()
         .setTitle('Interaktiv API')
